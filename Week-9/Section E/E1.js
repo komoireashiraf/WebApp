a2b0c05767d5bea0c05ba69728b2f0264c64b9d2
Komoire Ashiraf <127754423+komoireashiraf@users.noreply.github.com>
2024-11-14 08:56:37 +0300

    Add files via upload

---
+++ Week-9/Section E/E1.js
@@ -0,0 +1,11 @@
+// Assuming the rainfall data is stored in an array called rainfallData
+const rainfall_data = [15.2, 18.7, 22.1, 12.5, 19.8, 10.3, 25.6, 17.4, 21.9, 13.2];
+
+// Print the header
+console.log("Day Rainfall (mm) in Ghana ");
+
+// Use a for loop to iterate over the rainfall data
+for (let n = 0; n < rainfall_data.length; n++) {
+    // Print the day number and rainfall value
+    console.log(rainfall_data[n],rainfall_data.length);
+}
