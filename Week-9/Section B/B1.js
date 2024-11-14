9c7a913fb4132311a4218851f2c5dd186d9b871b
Komoire Ashiraf <127754423+komoireashiraf@users.noreply.github.com>
2024-11-14 08:53:03 +0300

    Add files via upload

---
+++ Week-9/Section B/B1.js
@@ -0,0 +1,11 @@
+// Role of Integreted Development Environment in JavaScript programming
+
+//Helps to highlight the errors in the javascript code written 
+//Helps to debug the errors in the javascript code written
+//Helps to enhance productivity by offering the code completion to ease the process
+
+
+// Examples of IDE using for coding
+
+//Visual Studio Code
+//IntelliJ IDEA
---
+++ Week-9/Section B/B2.js
@@ -0,0 +1,7 @@
+// Steps taken when installing javascript in visual studio
+
+// Open Visual Studio Code 
+// the left-side will appear a menu with various options like Explorer, Search, Source Control and many others
+//Select the option of Extensions, where you can search in the search-bar under Extensions for JavaScript and 
+//Install the JavaScript extension to support the work to be done in JavaScript language
+// 
